e505aa805fdf1c010a4023b6a6e34ad963840399
Celvindev <celvin.alfino32@gmail.com>
2024-05-06 16:30:12 +0700

    update baru

--- src/ui/home/Contxt4.jsx
+++ src/ui/home/Contxt4.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import buttoncard from '../../assets/home/iconarrowup.svg'
+import "../../pages/home/Home.css";
 
 const Contxt4 = () => {
     return (
         <>
             <div className="flex w-full justify-center mt-5">
-                <div className="grid w-[100%] h-[248px] bgrd bg-gray-925 rounded-[33px] place-items-center shadow">Content4</div>
+                <div className="grid w-[100%] h-[248px] bgrd bg-gray-925 rounded-[33px] place-items-center shadow">
+                    <img src={buttoncard} alt="" />
+                </div>
             </div>
         </>
     )
