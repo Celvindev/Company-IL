271da67ae3255b8c7c826f2f650ea44a176b5831
Celvindev <celvin.alfino32@gmail.com>
2024-05-08 10:44:25 +0700

    update

--- src/ui/home/Contxt3.jsx
+++ src/ui/home/Contxt3.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import ibm from '../../assets/home/ibm.png';
-import kamdek from '../../assets/home/kamdek.png';
-import red from '../../assets/home/red.png';
-import unreal from '../../assets/home/unreal.png';
 
 const Contxt3 = () => {
     return (
@@ -10,38 +6,38 @@ const Contxt3 = () => {
             <div className="grid-container">
                 <div className="grid w-[615px] h-[197px] mt-5 bgrd bg-gray-925 rounded-[33px] place-items-center shadow relative">
                     <div className="w-[615px] h-[35px] justify-center items-center gap-8 inline-flex relative moving-container">
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
-                        <img src={ibm} alt="Gambar 1" className="moving-image w-[80px]" />
-                        <img src={kamdek} alt="Gambar 2" className="moving-image w-[61px]" />
-                        <img src={red} alt="Gambar 3" className="moving-image w-[139px]" />
-                        <img src={unreal} alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
+                        <img src='/assets/home/ibm.png' alt="Gambar 1" className="moving-image w-[80px]" />
+                        <img src='/assets/home/kamdek.png' alt="Gambar 2" className="moving-image w-[61px]" />
+                        <img src='/assets/home/red.png' alt="Gambar 3" className="moving-image w-[139px]" />
+                        <img src='/assets/home/unreal.png' alt="Gambar 4" className="moving-image w-[43px]" />
                     </div>
                 </div>
             </div>
--- src/ui/home/Contxt5.jsx
+++ src/ui/home/Contxt5.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 
 const Contxt5 = () => {
     const [isDown, setIsDown] = useState(false);
@@ -21,37 +21,78 @@ const Contxt5 = () => {
     };
 
     const handleMouseMove = (e) => {
-        if (!isDown) return;
+        if (!isDown) {
+            return;
+        }
         e.preventDefault();
         const x = e.pageX - sliderRef.current.offsetLeft;
         const walk = (x - startX) * 3;
         sliderRef.current.scrollLeft = scrollLeft - walk;
     };
+
+    const handleMouseScroll = (e) => {
+        if (!isDown) {
+            const delta = Math.max(-1, Math.min(1, (e.nativeEvent.wheelDelta || -e.nativeEvent.detail)));
+            sliderRef.current.scrollLeft -= delta * 40;
+        }
+    };
+
     return (
         <>
             <div className="flex w-full justify-center mt-5">
                 <div className="bgrd w-[100%]  h-[517px]  rounded-[33px]">
                     <h1 className="text-left p-10 text-white orpro">Our Program</h1>
-                    <div className="grid w-[100%] px-10 place-items-center shadow"
+                    <div
+                        className="grid w-[100%] px-10 place-items-center shadow"
                         ref={sliderRef}
                         onMouseDown={handleMouseDown}
                         onMouseLeave={handleMouseLeave}
                         onMouseUp={handleMouseUp}
                         onMouseMove={handleMouseMove}
-                        style={{ overflowY: 'auto', overflowX: 'hidden', display: 'grid', gridAutoFlow: 'column', gap: '1rem' }}
+                        onWheel={handleMouseScroll}
+                        style={{
+                            overflowY: 'auto',
+                            overflowX: 'hidden',
+                            display: 'grid',
+                            gridAutoFlow: 'column',
+                            gap: '1rem',
+                        }}
                     >
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 1</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 2</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 3</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 4</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 5</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 6</div>
-                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">Card 7</div>
+
+                        <div className="card cardhome w-[401px] h-[357px]  rounded-[44px] image-full">
+                            <figure><img src="/assets/home/card1.png" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 2
+                        </div>
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 3
+                        </div>
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 4
+                        </div>
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 5
+                        </div>
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 6
+                        </div>
+                        <div className="w-[401px] h-[357px] bg-NeonWhite rounded-[44px] p-10">
+                            Card 7
+                        </div>
                     </div>
                 </div>
-            </div >
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Contxt5
+export default Contxt5;
