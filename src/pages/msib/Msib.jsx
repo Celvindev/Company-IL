cf70673097a672cc9c1d6b164400eed100250f2c
Celvindev <celvin.alfino32@gmail.com>
2024-05-13 10:46:57 +0700

    update celvin

--- src/pages/msib/Msib.jsx
+++ src/pages/msib/Msib.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import "../../pages/msib/Msib.css";
 import Navbar from '../../components/header/Navbar'
+import Contxt1 from '../../ui/msib/Contxt1'
+import Contxt2 from '../../ui/msib/Contxt2';
 
 const Msib = () => {
     return (
         <>
             <Navbar />
-            <div className='flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-6 mx-auto max-w-screen-xl'>
+            <div className='flex flex-col items-center justify-center py-6 mx-auto max-w-screen-xl'>
                 <div className="flex w-full justify-center">
                     <div className="mr-5 w-[100%]">
+                        <Contxt1 />
 
                     </div>
-
+                    <Contxt2 />
                 </div>
 
             </div >
---
+++ src/ui/msib/Contxt2.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Contxt2 = () => {
+    return (
+        <>
+            <div className="mr-5 w-[100%]">
+                <div className="grid w-[100%] h-[541.38px] place-items-center ">
+                    <img className="w-[100%]" src='assets/msib/learms.svg' />
+                    <div className="card w-[360px] h-[393px] -ml-40 crdminews">
+                        <div className="card-body">
+                            <h2 className="card-title text-white">Card title!</h2>
+
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">See more <i className="fa-solid fa-arrow-right ml-2"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Contxt2
