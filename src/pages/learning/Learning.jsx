368d305fe285581396157a4cd8a2d49ae2c8739d
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-13 16:30:04 +0700

    update learning

--- src/pages/learning/Learning.jsx
+++ src/pages/learning/Learning.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import "../../pages/learning/Learning.css";
 import Navbar from '../../components/header/Navbar'
-import Contxt1 from '../../ui/home/Contxt1';
+import Contxt1 from '../../ui/learning/Contxt1';
+import Contxt2 from '../../ui/learning/Contxt2';
+import Contxt3 from '../../ui/learning/Contxt3';
 
 const Learning = () => {
   return (
     <>
       <Navbar />
-      <div className='flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-6 mx-auto max-w-screen-xl'>
-        <div className="flex w-full justify-center">
-          <div className="mr-5 w-[100%]">
-          <Contxt1 />
-          </div>
-        </div>
-      </div >
+      <div className='flex flex-col items-center justify-center py-6 mx-auto max-w-screen-xl'>
+                <div className="flex w-full justify-center mb-40">
+                    <div className="mr-5 w-[100%]">
+                        <Contxt1 />
+
+                    </div>
+                    <Contxt2 />
+                </div>
+                <Contxt3 />
+            </div >
     </>
   )
 }
--- src/ui/learning/Contxt1.jsx
+++ src/ui/learning/Contxt1.jsx
@@ -1,22 +1,85 @@
 import React from 'react'
-import "../../pages/home/Home.css";
+import "../../pages/learning/Learning.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import Kampus from '../../assets/msib/yy3.png';
+import Game from '../../assets/learning/game.png';
+import Manage from '../../assets/learning/manage.png';
+import Coding from '../../assets/learning/coding.png';
+import Ai from '../../assets/learning/ai.png';
+import D from '../../assets/learning/3d.png';
 
 const Contxt1 = () => {
     return (
         <>
-            <div className="grid w-[615px] h-[511px] bgrd bg-gray-925 rounded-[33px] px-10 py-8 shadow ">
-                <h1 className=" text-white text-[35px] txtab-c">Join Infinite Learning<br /> and Show Your Creativity!</h1>
-                <p className=" text-white -mt-5 text-[14px] leading-normal">Infinite Learning, a division of PT Kinema Systrans multimedia
-                    <br />(a subsidiary of Infinite Studios) under Citra Agramasinti Nusantara
-                    (Citramas Group).<br />...</p>
-                <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient">
-                    <p className=" msbtext">MSIB<i className="fa-solid fa-arrow-right ml-2"></i></p>
-                </button>
-                <button className="w-[460px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient">
-                    <p className=" msbtext">Regular Courses<i className="fa-solid fa-arrow-right ml-2"></i> </p>
-                </button>
-            </div>
+            <div className="grid w-[599px] h-[287px] bg-gray-925 rounded-[33px] py-8 shadow ">
+                <h1 className=" text-white text-2xl font-semibold font-['Montserrat'] leading-7">#Anyoneanything</h1>
+                <h1 className=" text-white text-[64px] txtmsib-c">Start your <br /> journey with us</h1>
+                    <label className="input input-bordered mt-10 h-[60px] btn btcat btn-outline w-[45%] rounded-full ">
+                        <input type="text" className="grow font-normal font-['Montserrat'] text-[18px] " placeholder="Find the course you need " />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    </label>
+                
 
+                <h1 className=" text-white text-2xl font-semibold font-['Montserrat'] leading-7 mt-20">Categories</h1>
+                
+                <ul className="hidden lg:flex mt-1 gap-5">
+                    <li>
+                        <button className="w-[320px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Kampus} alt="km" />
+                            <p className="msibtext ml-5">Studi Independent</p>
+                        </button>
+                    </li>
+                
+                    <li>
+                        <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Kampus} alt="km" />
+                            <p className="msibtext ml-5">Magang</p>
+                        </button>
+                    </li>
+                </ul>
+
+                 
+
+ 
+                <ul className="hidden lg:flex mt-1 gap-5">
+                    <li>
+                        <button className="w-[220px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Coding} alt="km" />
+                            <p className="msibtext ml-5">Coding</p>
+                        </button>
+                    </li>
+                
+                    <li>
+                        <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Manage} alt="km" />
+                            <p className="msibtext ml-5">Management</p>
+                        </button>
+                    </li>
+
+                    <li>
+                        <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Game} alt="km" />
+                            <p className="msibtext ml-5">Game</p>
+                        </button>
+                    </li>
+
+                    <li>
+                        <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={D} alt="km" />
+                            <p className="msibtext ml-5">3D Design </p>
+                        </button>
+                    </li>
+
+                    <li>
+                        <button className="w-[260px] h-[90px] rounded-[44px] justify-center items-center flex border border-gradient mt-10">
+                            <img src={Ai} alt="km" />
+                            <p className="msibtext ml-5">A.I</p>
+                        </button>
+                    </li>
+                </ul>
+
+               
+            </div>
         </>
     )
 }
