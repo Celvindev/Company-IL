733512935f927dc40a24440c6d7d81f02bd50a3a
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-14 16:34:17 +0700

    update msib

--- src/ui/msib/Contxt2.jsx
+++ src/ui/msib/Contxt2.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const Contxt2 = () => {
     return (
         <>
-            <div className="mr-5 w-[100%]">
+            <div className="mr-5 w-[100%] ">
                 <div className="grid w-[100%] h-[541.38px] place-items-center ">
                     <img className="w-[100%]" src='assets/msib/learms.svg' />
-                    <div className="card w-[360px] h-[393px] -ml-40 crdminews">
+                    <div className="card w-[400px] h-[393px] -ml-40 crdminews breadcrumbs">
                         <div className="artboard">
                             <ul>
                                 <li>
-                                    <button className="w-[140px] h-[20px] rounded-[44px] items-center ml-5 mt-5 btn btcat btn-outline"> <p className="msibtxtcont">They Say</p></button>
+                                    <button className="w-[140px] h-[20px] rounded-[44px] items-center ml-5 mt-5 btn btcat btn-outline"> 
+                                        <p className="msibtxtcont">They Say</p>
+                                    </button>
                                 </li>
                                 
                                 <li> 
@@ -29,7 +31,42 @@ const Contxt2 = () => {
                                         </p>
                                     </button>   
                                 </li>   
+
+                                <li> 
+                                    <button className="w-[320px] h-[180px] rounded-[20px] items-center ml-5 mt-10 btn btn-outline "> 
+                                        <p className="msibcrdtxt text-left ">
+                                            <b className="cardtitle mr-30">Adi Prasetyo 
+                                            </b>
+                                            <div className="rating rating-xs">
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            </div> <br /> <br />
+                                            Infinite Learning menyediakan kursus yang luar biasa! Materi pelajarannya sangat terstruktur dengan baik dan disampaikan dengan jelas. Saya sangat terkesan dengan kualitas pembelajaran yang ditawarkan</p>
+                                    </button>   
+                                </li>   
+
+                                <li> 
+                                    <button className="w-[320px] h-[180px] rounded-[20px] items-center ml-5 mt-10 btn btn-outline "> 
+                                        <p className="msibcrdtxt text-left ">
+                                            <b className="cardtitle mr-30">Ahmad Fauzi 
+                                            </b>
+                                            <div className="rating rating-xs">
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            <input type="radio" name="rating-5" className="mask mask-star-2 bg-white-800" />
+                                            </div> <br /> <br />
+                                            Pengalaman saya dengan Infinite Learning sungguh fantastis! Mereka memiliki beragam kursus yang relevan dengan kebutuhan pasar saat ini. Instruktur nya sangat berpengetahuan dan responsif.</p>
+                                    </button>   
+                                </li>   
                             </ul>
+                            <button className="w-[140px] h-[20px] rounded-[44px] items-right ml-56 mt-5 btn btcat btn-outline"> 
+                                <p className="msibtxtcont">See More <i className="fa-solid fa-arrow-right ml-2"></i></p>
+                            </button>
                         </div>
                     </div>
                 </div>
