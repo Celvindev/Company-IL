57e44a94d114e8b9dc545ecce5a9fc8b07ad02ca
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-20 10:49:33 +0700

    update link about

--- src/ui/home/Contxt1.jsx
+++ src/ui/home/Contxt1.jsx
@@ -9,7 +9,7 @@ const Contxt1 = () => {
                 <h1 className=" text-white w-[350px] sm:w-full leading-[30px] sm:leading-[40px] text-2xl sm:text-[35px] txtab-c">Join Infinite Learning<br /> and Show Your Creativity!</h1>
                 <p className=" text-white w-[300px] sm:w-[440px] mt-2 sm:-mt-5 text-base sm:text-[14px] font-['Montserrat'] leading-normal">Infinite Learning, a division of PT Kinema Systrans multimedia
                     (a subsidiary of Infinite Studios) under Citra Agramasinti Nusantara
-                    (Citramas Group).<br />...</p>
+                    (Citramas Group).</p>
                 <Link to="/msib" className="button-link">
                     <button className="w-[90%] sm:w-[260px] h-[70px] sm:h-[90px] rounded-[44px] justify-center items-center flex border border-gradient">
                         <p className=" msbtext text-[20px] sm:text-[25px]">MSIB<i className="fa-solid fa-arrow-right ml-2"></i></p>
