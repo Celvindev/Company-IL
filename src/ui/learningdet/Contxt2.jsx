0e1132b99753e69594ec7db84c1b28437d6fe783
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-20 16:30:11 +0700

    update learningdet

--- src/ui/learningdet/Contxt2.jsx
+++ src/ui/learningdet/Contxt2.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const Contxt2 = () => {
     return (
         <>
-            
+           
+            <div className="artboard h-[600px]">
+            <div className="grid w-[100%] h-[400px] place-items-center mt-40">
+                <button className="w-[400px] h-[128px] rounded-[82px] justify-center items-center flex border border-gradient ml-10">
+                    <p className="msibtext text-[16px]">Why You Should Learn</p>
+                </button>
+
+                <button className="mt-16 w-[400px] h-[128px] rounded-[82px] justify-center items-center flex border border-gradient ml-10">
+                    <p className="msibtext text-[16px]">Requirement</p>
+                </button>
+
+                <button className="mt-16 w-[400px] h-[128px] rounded-[82px] justify-center items-center flex border border-gradient ml-10">
+                    <p className="msibtext text-[16px]">Start Date - End Program</p>
+                </button>
+                </div>
+
                 <div className="grid w-[100%] h-[400px] place-items-center mt-40">
                 <button className="w-[400px] h-[128px] rounded-[82px] justify-center items-center flex border border-gradient ml-10">
                     <p className="msibtext text-[16px]">Why You Should Learn</p>
@@ -17,6 +32,7 @@ const Contxt2 = () => {
                     <p className="msibtext text-[16px]">Start Date - End Program</p>
                 </button>
                 </div>
+            </div>
         </>
     )
 }
