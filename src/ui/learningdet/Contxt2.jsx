1ead9e9e541f504d23fd044e186d81406e9c9767
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-20 16:32:09 +0700

    update dikit

--- src/ui/learningdet/Contxt2.jsx
+++ src/ui/learningdet/Contxt2.jsx
@@ -4,8 +4,8 @@ const Contxt2 = () => {
     return (
         <>
            
-            <div className="artboard h-[600px]">
-            <div className="grid w-[100%] h-[400px] place-items-center mt-40">
+            <div className="artboard h-[550px] mt-40">
+            <div className="grid w-[100%] h-[400px] place-items-center ">
                 <button className="w-[400px] h-[128px] rounded-[82px] justify-center items-center flex border border-gradient ml-10">
                     <p className="msibtext text-[16px]">Why You Should Learn</p>
                 </button>
