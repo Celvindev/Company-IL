d03a1b0173a7a8c6a31dbd4fc60a3319cc04bb88
utiigusnia <gusnia21ti@mahasiswa.pcr.ac.id>
2024-05-29 10:41:28 +0700

    update learningdet

--- src/ui/learningdet/Contxt2.jsx
+++ src/ui/learningdet/Contxt2.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Contxt2 = () => {
+    
+    const [programType, setProgramType] = useState('why');
     return (
         <>
            
             <div className="artboard hidden sm:block sm:h-[450px] mt-32">
             <div className="grid w-[100%] h-[400px] place-items-center ">
-                <button className="w-[380px] h-[100px] rounded-[46px] justify-center items-center flex border border-gradient ml-10">
+                <button className="w-[380px] h-[100px] rounded-[46px] justify-center items-center flex border border-gradient ml-10" onClick={() => setProgramType('why')}>
                     <p className="msibtext text-[22px]">Why You Should Learn</p>
                 </button>
 
-                <button className="mt-16 w-[380px] h-[100px] rounded-[46px] justify-center items-center flex border border-gradient ml-10">
+                <button className="mt-16 w-[380px] h-[100px] rounded-[46px] justify-center items-center flex border border-gradient ml-10" onClick={() => setProgramType('require')}>
                     <p className="msibtext text-[22px]">Requirement</p>
                 </button>
 
